3e3e5d793e39ccd3501808b2b83a35d0c86f3991
koolsb <14332595+koolsb@users.noreply.github.com>
2022-02-15 15:58:20 -0600

    fix log background job

--- backgroundJobs.php
+++ backgroundJobs.php
@@ -98,3 +98,14 @@ function updateRemotePlaylist($remotePlaylist, $apiKey, $newTime) {
     logEntry("Remote Playlsit Automatic Update Failed");
   }
 }
+
+function logEntry($data) {
+
+	global $logFile,$myPid;
+
+	$data = $_SERVER['PHP_SELF']." : [".$myPid."] ".$data;
+	
+	$logWrite= fopen($logFile, "a") or die("Unable to open file!");
+	fwrite($logWrite, date('Y-m-d h:i:s A',time()).": ".$data."\n");
+	fclose($logWrite);
+}
