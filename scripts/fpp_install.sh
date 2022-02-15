09adffe4a55f3444b12fe4821ab595f60ea8a66d
koolsb <14332595+koolsb@users.noreply.github.com>
2022-02-14 18:24:40 -0600

    fix install script

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -3,4 +3,4 @@
 # fpp-plugin-Template install script
 
 # Mark to reboot
-sed -i -e "s/^restartFlag .*/restartFlag = 1/" ${FPPHOME}/media/settings
+sed -i -e "s/^restartFlag .*/restartFlag = 1/" /home/fpp/media/settings
