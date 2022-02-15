337d03c744454835b275edc58b3583aa34bd9fbf
koolsb <14332595+koolsb@users.noreply.github.com>
2022-02-15 16:25:14 -0600

    delete unused commands

--- commands/stop_remote_falcon.php
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env php
-<?php
-$skipJSsettings=true;
-include_once "/opt/fpp/www/config.php";
-include_once "/opt/fpp/www/common.php";
-$pluginName = "remote-falcon";
-
-WriteSettingToFile("remote_fpp_enabled",urlencode("false"),$pluginName);
-?>
